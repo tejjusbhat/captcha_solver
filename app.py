936f420035a63c232e10e45e309c1476c00cf5b5
tejjus <tejjus.21b1541044@abes.ac.in>
2024-02-17 21:42:26 +0530

    adds a dorm to send image to backend

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, send_file
+import cv2
+from captcha_solver import solve_captcha
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST' and "image" in request.files:
+        image = request.files['image']
+        img = cv2.imdecode(image.read(), cv2.IMREAD_COLOR)
+
+        annotated = solve_captcha(img, "traffic light")
+
+        return send_file(annotated, mimetype='image') 
     return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
--- captcha_solver.py
+++ captcha_solver.py
@@ -19,14 +19,5 @@ def solve_captcha(image, target):
     label = f'{model.names[int(cls)]} {conf:.2f}'
     image = cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
     image = cv2.putText(image, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (36,255,12), 2)
-  
+
   return image
-
-
-original = cv2.imread('images/2.png')
-
-annotated = solve_captcha(original, "traffic light")
-
-cv2.imshow('annotated', annotated)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
