44f646a416ba0d9ebadc9347bab62041e5733c81
tejjus <tejjus.21b1541044@abes.ac.in>
2024-02-17 23:32:10 +0530

    tries to fix display issue

--- app.py
+++ app.py
@@ -6,24 +6,24 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST' and "image" in request.files:
-        image = request.files['image']
-        img = cv2.imdecode(image.read(), cv2.IMREAD_COLOR)
+  if request.method == 'POST' and "image" in request.files:
+    file = request.files['image']
+    img = cv2.imdecode(file.read() ,cv2.IMREAD_COLOR)
 
-        annotated = solve_captcha(img, "traffic light")
+    annotated = solve_captcha(img, "traffic light")
 
-        _, buffer = cv2.imencode('.png', annotated)
+    _, buffer = cv2.imencode('.png', annotated)
 
-        image_bytes = buffer.tobytes()
+    image_bytes = buffer.tobytes()
 
-        return send_file(
-            filename_or_fp=None,
-            mimetype='image/png',
-            attachment_filename='annotated_image.png',
-            data=image_bytes
-        )
-    return render_template('index.html')
+    return send_file(
+      filename_or_fp=None,
+      mimetype='image/png',
+      attachment_filename='annotated_image.png',
+      data=image_bytes
+      )
+  return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+  app.run(debug=False)
